af446db2e3852efa6b4ff7ad51997a1f37601577
zwh <z_wh@sina.cn>
2018-12-01 13:32:01 +0800

    第一次提交

---
+++ js/remfont.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    setHtmlFontSize();
+    showBody();
+};
+
+window.addEventListener("resize", function () {
+    setHtmlFontSize();
+});
+
+function setHtmlFontSize(){
+    var htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    var htmlDOM = document.getElementsByTagName("html")[0];
+    htmlDOM.style.fontSize = htmlWidth / 10 + "px";
+}
+
+function showBody() {
+    document.getElementsByTagName("body")[0].style.visibility = "visible";
+}
